140dab0bed538056534efec0de9cbe89380ebe67
Cheng Tan <noinil@gmail.com>
2016-05-04 18:24:20 +0900

    add setup.py for PyPI.

---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='py-sdvb',
+    version='1.5',
+
+    description='Dictionary interface of stardict, support regular expression (regex) search and colored output.',
+    long_description=long_description,
+
+    url='https://github.com/noinil/py_sdvb',
+
+    author='Cheng Tan',
+    author_email='noinil@gmail.com',
+
+    license='GPL3',
+
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Foreign Language Learner; Chinese',
+        'Topic :: Vocabulary Builder :: Dictionary Tools',
+
+        'License :: GPL3 License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='dictionary, stardict, vocabulary builder, English, Chinese',
+
+    packages = ['pysdvb'],
+
+    install_requires=['pystardict'],
+
+    entry_points={
+        'console_scripts': [
+            'pysdcv=sdcv:translate_word',
+            'pysdvb=sdvb:main',
+        ],
+    },
+)
